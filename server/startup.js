add99434aa40817c73e7524a0cb67975d2e4160f
Steel Jones <steel.jones@gmail.com>
2016-06-06 17:33:41 -0400

    Add bgb route

--- server/startup.js
+++ server/startup.js
@@ -1,1 +1,4 @@
-BrowserPolicy.framing.allowAll();
+//BrowserPolicy.framing.allowAll();
+BrowserPolicy.content.allowFrameOrigin("ats.mtgarchitect.com");
+BrowserPolicy.content.allowFrameOrigin("openinnovation.astrazeneca.com");
+BrowserPolicy.content.allowFrameOrigin("clinicaltrials.gov");
