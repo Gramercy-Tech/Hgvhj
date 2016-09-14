97311a30fb5b794d7b5134724e454899261b2d21
Steel Jones <steel.jones@gmail.com>
2016-09-14 10:40:56 -0400

    Add pipeline2016 route

---
+++ client/lib/references2016.js
@@ -0,0 +1,52 @@
+references2016 = `
+AstraZeneca Pharmaceuticals LP. Development Pipeline as of July 28, 2016. AstraZeneca website. Accessed September 2016.
+
+Barnes P J. Corticosteroid resistance in patients with asthma and chronic obstructive pulmonary disease. <em>J Allergy Clin Immunol.</em> 2013;131(3):637.
+
+Barnes P J. Scientific rationale for inhaled combination therapy with long-acting b2-agonists and corticosteroids. <em>Eur Respir J.</em> 2002;19:183-184.
+
+Busse WW, Molfino NA, Kolbeck R.  Interleukin-5 receptor-directed strategies. In: Lee JJ, Rosenberg HF, eds. <em>Eosinophils in Health and Disease.</em> London, UK: Academic Press; 2013: 587-591.
+
+Cazzola M, Lopez-Campos J, Puente-Maestu L. The MABA approach: a new option to
+improve bronchodilator therapy. <em>Eur Respir J.</em> 2013;42: 885-886.
+
+De Bosscher K, Haegeman G, Elewaut D. Targeting inflammation using selective glucocorticoid receptor Modulators. <em>Curr Opin Pharmacol.</em> 2010;10(4):498,502.
+
+Gaffey K, Reynolds S, Plumb J, Kaur M, Singh D. Increased phosphorylated p38 mitogenactivated protein kinase in COPD lungs. <em>Eur Respir J.</em> 2013; 42:32.
+
+Gauvreau G M, O’Byrne P M, Boulet L, et al. Effects of an Anti-TSLP Antibody on
+Allergen-Induced Asthmatic Responses. <em>N Engl J Med.<em> 2014;370(22):2103.
+
+Ghazi A, Trikha A, Calhoun W J Benralizumab – a humanized mAb to IL-5Rα with enhanced antibody-dependent cell-mediated cytotoxicity – a novel approach for the treatment of asthma. <em>NIH Public Access Author Manuscript.</em> 2012:2.
+
+Krieg A M. Therapeutic potential of Toll-like receptor 9 activation. <em>Nature Rev Drug Discov.</em> 2006;5:447.
+
+Laine D I, Busch-Petersen J. Inhibitors of cathepsin C (dipeptidyl peptidase I). <em>Expert Opin Ther Pat.</em> 2010;10(4):498.
+
+Mardh C K, Gustavsson M, Smailagic A. Pharmacological Characterization Of Azd5423, An Inhaled Non-Steroidal Agonist Of The Glucocortiocoid Receptor. <em>Am J Respir Crit Care Med.</em> 2015;191:1.
+
+Molfino N A, Gossage D, Kolbeck R, Parker J M, Geba G P. Molecular and clinical rationale for therapeutic targeting of interleukin-5 and its receptor. <em>Clin Exp Allergy.</em> 2011;42:278.
+
+NIH U.S. National Library of Medicine TOXNET. Substance Name: Tezepelumab [USAN]. NIH U.S. National Library of Medicine TOXNET website. http://chem.sis.nlm.nih.gov/chemidplus/rn/1572943-04-4. Accessed September 2016.
+
+Noppert S J, Fitzgerald K A, Hertzog P J. The role of type I interferons in TLR responses. <em>Immuno and Biol.</em> 2007;85:446-448,450..
+
+Pearl Therapeutics Inc. Pearl website. https://www.pearltherapeutics.com/pipeline-overview. Accessed September 2016.
+
+Pelaia G, Maselli R, Gallelli L. Pharmacologic rationale, efficacy and safety of the fixed-dose co-formulation of indacaterol and glycopyrronium. <em>Multidiscip Respir Med.</em> 2014;9(64):2. 
+
+Pelaia G, Renda T, Gallelli L, et al. Molecular mechanisms underlying airway smooth
+muscle contraction and proliferation: Implications for asthma. <em>Respir Med.</em> 2008;102:1175.
+
+Pelaia G, Vatrella A, Maselli R. The potential of biologics for the treatment of asthma. <em>Nature Rev Drug Discov.</em> 2012;11:966.
+
+Piper E, Brightling C, Niven R, etal. A phase II placebo-controlled study of tralokinumab in moderate-to-severe asthma. <em>Eur Respir J.</em> 2013;41:330,335. 
+
+Rhen T, Cidlowski J A. Antiinflammatory Action of Glucocorticoids — New Mechanisms for Old Drugs. <em>N Engl J Med.</em> 2005;353(16):1712.
+
+Sundahl N, Bridelance J, Libert C, De Bosscher K, Beck I M. Selective glucocorticoid receptor modulation: New directions with non-steroidal scaffolds. <em>Pharmacol Ther.</em> 2015;152:29.
+
+Venkayya R, Lam M, Willkom M, Grünig G, Corry D B, Erle D J. The Th2 Lymphocyte Products IL-4 and IL-13 Rapidly Induce Airway Hyp erresponsiveness Through Direct Effects on Resident Airway Cells. <em>Am J Resir Cell Mol Biol.</em> 2002;26:202,207.
+
+Yang Y, Kim S C, Yu T. Functional Roles of p38 Mitogen-Activated Protein Kinase in Macrophage-Mediated Inflammatory Responses. <em>Mediators Inflamm.</em> 2014;2014:2-3.
+`
